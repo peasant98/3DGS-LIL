28103b047e635bc84f39b3975a351bb9414d02cb
Matt Strong <mastro1@stanford.edu>
2024-05-23 19:46:29 -0700

    add LEGaussians

--- perception.sh
+++ perception.sh
@@ -4,3 +4,18 @@ cd data/real-world/lab-scene
 unzip database.zip
 
 cd ../../..
+
+cd LEGaussians
+
+conda create -n legaussians python=3.8 -y
+conda activate legaussians
+
+pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116
+
+pip install tqdm plyfile timm open_clip_torch scipy six configargparse pysocks python-dateutil imageio seaborn opencv-python scikit-learn tensorboard Pillow==9.5.0
+
+# Install local packages from the 'submodules' directory
+# a modified gaussian splatting (+ semantic features rendering)
+cd submodules/diff-gaussian-rasterization/ && python -m pip install -e . && cd ../..
+# simple-knn
+cd submodules/simple-knn/ && python -m pip install -e . && cd ../..
